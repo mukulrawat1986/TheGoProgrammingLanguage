df5b6ed77f3375a5c5eb3c55a82dfe86e075ae5d
Mukul Rawat <mukulsrawat@gmail.com>
2016-08-08 23:19:19 +0530

    exercise 1.1 done

---
+++ src/exercises/gopl.io/ch1/1/main.go
@@ -0,0 +1,16 @@
+// Modify the echo progam to also print os.Args[0]
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
