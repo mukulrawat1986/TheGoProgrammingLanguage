3de90a9e4e30273f246fc5a56bc94e372c8d27b8
Mukul Rawat <mukulsrawat@gmail.com>
2016-08-11 17:14:56 +0530

    started exercise 1.8

---
+++ src/exercises/gopl.io/ch1/8/main.go
@@ -0,0 +1,30 @@
+// Modify Fetch to add the prefix http:// to each argument url if it is missing
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// check if the url has the prefix, if not, add it
+		if res := strings.HasPrefix(url, "http://"); !res {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		_, err := io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
