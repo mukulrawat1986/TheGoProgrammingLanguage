155946488720c238ec85e7cd8081373f3c242791
Mukul Rawat <mukulsrawat@gmail.com>
2016-08-11 17:20:41 +0530

    finished exercise 1.9

---
+++ src/exercises/gopl.io/ch1/9/main.go
@@ -0,0 +1,31 @@
+// modify Fetch to also print HTTP status code found in resp.Status
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// check if url has the prefix "http://", if not, add it
+		if res := strings.HasPrefix(url, "http://"); !res {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		fmt.Printf("Status code: %s\n", resp.Status)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
