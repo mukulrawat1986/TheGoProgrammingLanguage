40ea365cdf27e5cc1389fd7acdb773bd71fd8d3f
Mukul Rawat <mukulsrawat@gmail.com>
2016-08-19 17:24:07 +0530

    echo server that simulates the reverberations of a real echo

---
+++ src/examples/gopl.io/ch8/reverb1/main.go
@@ -0,0 +1,44 @@
+// An echo server that tries to simulate the reverberations of a real echo
+package main
+
+import (
+	"time"
+	"net"
+	"log"
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // eg. connection aborted
+			continue
+		}
+		go handleConn(conn) // create a new goroutine for each connection
+	}
+}
+
+func handleConn(c net.Conn){
+	defer c.Close()
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1 * time.Second)
+	}
+	// Note: ignoring errors from input.Err()
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+}
